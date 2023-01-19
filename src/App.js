2d87ef71c42d386b3a206b6a662412530c71567a
Andres Bolivar <dre.bolivar@gmail.com>
2023-01-19 13:33:41 -0500

    added remove button

--- src/App.js
+++ src/App.js
@@ -37,23 +37,26 @@ function App() {
           <div className="recipe" key={recipe.id}>
             <h3>{recipe.title}</h3>
             <p dangerouslySetInnerHTML={{ __html: recipe.desc }}></p>
-            <div>
-              <h4>Ingredients</h4>
-              <ul>
-                {recipe.ingredients.map((ingredient, i) => (
-                  <li key={i}>{ingredient}</li>
-                ))}
-              </ul>
-              <h4>Steps</h4>
-              <ol>
-                {recipe.steps.map((step, i) => (
-                  <li key={i}>{step}</li>
-                ))}
-              </ol>
-            </div>
+            {recipe.viewing && (
+              <div>
+                <h4>Ingredients</h4>
+                <ul>
+                  {recipe.ingredients.map((ingredient, i) => (
+                    <li key={i}>{ingredient}</li>
+                  ))}
+                </ul>
+                <h4>Steps</h4>
+                <ol>
+                  {recipe.steps.map((step, i) => (
+                    <li key={i}>{step}</li>
+                  ))}
+                </ol>
+              </div>
+            )}
 
             <div className="buttons">
               <button>View {recipe.viewing ? 'less' : 'more'}</button>
+              <button className="remove">Remove</button>
             </div>
           </div>
         ))}
