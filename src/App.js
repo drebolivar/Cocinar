be9180009fdd7fdd8ee7476d16a08f0e145bb9d8
Andres Bolivar <dre.bolivar@gmail.com>
2023-01-19 13:18:05 -0500

    mapped to firebase db

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import { db } from './firebase.config'
 import { useState, useEffect } from 'react'
+import { collection, onSnapshot } from 'firebase/firestore'
 
 function App() {
+  const [recipes, setRecipes] = useState([])
+  const [form, setForm] = useState({
+    title: '',
+    desc: '',
+    ingredients: [],
+    steps: []
+  })
+  const [popupActive, setPopupActive] = useState(false)
+
+  const recipesCollectionRef = collection(db, 'recipes')
+
+  useEffect(() => {
+    onSnapshot(recipesCollectionRef, (snapshot) => {
+      setRecipes(
+        snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            viewing: false,
+            ...doc.data()
+          }
+        })
+      )
+    })
+  }, [])
+
   return (
     <div className="App">
       <h1>My Recipes</h1>
+      <button> Add Recipe</button>
+      <div className="recipes">
+        {recipes.map((recipe, i) => (
+          <div className="recipe" key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p dangerouslySetInnerHTML={{ __html: recipe.desc }}></p>
+            <div>
+              <h4>Ingredients</h4>
+              <ul>
+                {recipe.ingredients.map((ingredient, i) => (
+                  <li key={i}>{ingredient}</li>
+                ))}
+              </ul>
+              <h4>Steps</h4>
+              <ol>
+                {recipe.steps.map((step, i) => (
+                  <li key={i}>{step}</li>
+                ))}
+              </ol>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
